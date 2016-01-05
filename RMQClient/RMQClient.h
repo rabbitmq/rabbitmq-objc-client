7d365350fd303e06a8f260936d5a9ea9ec3088ad
Andrew Bruce <me@andrewbruce.net>
2016-01-05 09:54:42 +0000

    Initial Commit

---
+++ RMQClient/RMQClient.h
@@ -0,0 +1,19 @@
+//
+//  RMQClient.h
+//  RMQClient
+//
+//  Created by Pivotal on 05/01/2016.
+//  Copyright © 2016 Pivotal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RMQClient.
+FOUNDATION_EXPORT double RMQClientVersionNumber;
+
+//! Project version string for RMQClient.
+FOUNDATION_EXPORT const unsigned char RMQClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RMQClient/PublicHeader.h>
+
+
