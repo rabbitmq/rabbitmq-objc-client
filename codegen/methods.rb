98d211b19ef341d641f965fb9cf1c067bda132c0
Andrew Bruce <me@andrewbruce.net>
2016-02-08 13:18:20 +0000

    Create ctors for outgoing methods
    [#13] [#15]

--- codegen/methods.rb
+++ codegen/methods.rb
@@ -17,6 +17,15 @@ def from_this_dir(path)
   File.absolute_path(path, __dir__)
 end
 
+def colon_aligned_name(first_line, name)
+  to_colon, _ = first_line.split(':')
+  "#{name}:".rjust(to_colon.length + 1)
+end
+
+def property_type_and_label(field)
+  "(nonnull #{field[:type]} *)#{field[:name]}"
+end
+
 File.open(from_this_dir("amqp0-9-1.extended.xml")) do |f|
   xml = Nokogiri::XML(f)
   template = ERB.new(File.read(from_this_dir('template.erb')), nil, '-')
@@ -40,10 +49,23 @@ File.open(from_this_dir("amqp0-9-1.extended.xml")) do |f|
                  f[:type].underscore.camelize
                end
         {
-          type: type,
+          type: "AMQ#{type}",
           name: f[:name].underscore.camelize(:lower),
         }
       }
+
+      constructor =
+        if method.xpath('chassis').first[:name] == 'client'
+          ""
+        elsif fields.any?
+          first_field_name = "#{fields[0][:name][0].upcase}#{fields[0][:name][1..-1]}:"
+          first_line = "- (nonnull instancetype)initWith#{first_field_name}#{property_type_and_label(fields[0])}"
+          constructor_rest = fields[1..-1].map { |field|
+            "#{colon_aligned_name(first_line, field[:name])}#{property_type_and_label(field)}"
+          }
+          "#{first_line}\n#{constructor_rest.join("\n")};"
+        end
+
       puts template.result(binding)
     end
   end
