716b59c4966cb169e55ee143c40bdf3dee44143e
Andrew Bruce <me@andrewbruce.net>
2016-02-10 20:51:54 +0000

    Don't use hex unnecessarily

--- RMQClient/AMQProtocolHeader.m
+++ RMQClient/AMQProtocolHeader.m
@@ -6,10 +6,10 @@
 - (NSData *)amqEncoded {
     char *buffer = malloc(8);
     memcpy(buffer, "AMQP", strlen("AMQP"));
-    buffer[4] = 0x00;
-    buffer[5] = 0x00;
-    buffer[6] = 0x09;
-    buffer[7] = 0x01;
+    buffer[4] = 0;
+    buffer[5] = 0;
+    buffer[6] = 9;
+    buffer[7] = 1;
     return [NSData dataWithBytesNoCopy:buffer length:8];
 }
 
