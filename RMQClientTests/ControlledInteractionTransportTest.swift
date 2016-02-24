26382b6812f14ca1cb6e4e5894fa8694388df7c8
Andrew Bruce <me@andrewbruce.net>
2016-02-24 13:29:43 +0000

    Do what the Travis build failure told me to do
    This wasn't failing locally, but ho hum.
    
    https://travis-ci.org/camelpunch/RMQClient/builds/111463308

--- RMQClientTests/ControlledInteractionTransportTest.swift
+++ RMQClientTests/ControlledInteractionTransportTest.swift
@@ -18,7 +18,7 @@ class ControlledInteractionTransportTest: RMQTransportContract {
     override func testSendingPreambleStimulatesAConnectionStart() {
         let tenthOfSecond = dispatch_time(DISPATCH_TIME_NOW, Int64(0.1 * Double(NSEC_PER_SEC)))
         dispatch_after(tenthOfSecond, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
-            transport.serverSendsPayload(MethodFixtures.connectionStart(), channelID: 1)
+            self.transport.serverSendsPayload(MethodFixtures.connectionStart(), channelID: 1)
         }
 
         super.testSendingPreambleStimulatesAConnectionStart()
