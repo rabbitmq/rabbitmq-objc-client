0060aedb31b2598bf54b5db5bdde10fd6f0e97a3
Andrew Bruce <me@andrewbruce.net>
2016-03-07 14:13:41 +0000

    Test copes with queues not running out of allocations
    [#35]

--- RMQClientTests/ChannelAllocationTest.swift
+++ RMQClientTests/ChannelAllocationTest.swift
@@ -1,6 +1,7 @@
 import XCTest
 
 class ChannelAllocationTest: XCTestCase {
+    let allocationsPerQueue = 30000
 
     func allocateAll(allocator: RMQChannelAllocator, _ sender: RMQSender) {
         for _ in 1...AMQChannelLimit {
@@ -25,45 +26,47 @@ class ChannelAllocationTest: XCTestCase {
     }
 
     func testNumbersAreNotDoubleAllocated() {
-        let sender = SenderSpy()
-        let allocator = RMQMultipleChannelAllocator()
-        var channels1 = Set<NSNumber>()
-        var channels2 = Set<NSNumber>()
-        var channels3 = Set<NSNumber>()
-        let queues = [
+        let sender      = SenderSpy()
+        let allocator   = RMQMultipleChannelAllocator()
+        var channelSet1 = Set<NSNumber>()
+        var channelSet2 = Set<NSNumber>()
+        var channelSet3 = Set<NSNumber>()
+        let group       = dispatch_group_create()
+        let queues      = [
             dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0),
             dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0),
             dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)
         ]
-        let group = dispatch_group_create()
 
         dispatch_group_async(group, queues[0]) {
-            for _ in 1...30000 {
-                channels1.insert(allocator.allocateWithSender(sender).channelID)
+            for _ in 1...self.allocationsPerQueue {
+                channelSet1.insert(allocator.allocateWithSender(sender).channelID)
             }
         }
 
         dispatch_group_async(group, queues[1]) {
-            for _ in 1...30000 {
-                channels2.insert(allocator.allocateWithSender(sender).channelID)
+            for _ in 1...self.allocationsPerQueue {
+                channelSet2.insert(allocator.allocateWithSender(sender).channelID)
             }
         }
 
         dispatch_group_async(group, queues[2]) {
-            for _ in 1...30000 {
-                channels3.insert(allocator.allocateWithSender(sender).channelID)
+            for _ in 1...self.allocationsPerQueue {
+                channelSet3.insert(allocator.allocateWithSender(sender).channelID)
             }
         }
 
         dispatch_group_wait(group, DISPATCH_TIME_FOREVER)
 
-        let expectedUniqueCannotAllocateCount = queues.count
+        let channelSets                    = [channelSet1, channelSet2, channelSet3]
+        let expectedUniqueUnallocatedCount = channelSets.reduce(0, combine: sumUnallocated)
+        let total                          = channelSets.reduce(0, combine: {$0 + $1.count})
 
-        XCTAssertEqual(
-            AMQChannelLimit + expectedUniqueCannotAllocateCount,
-            channels1.count + channels2.count + channels3.count,
-            "Got \(channels1.count), \(channels2.count) and \(channels3.count)"
-        )
+        XCTAssertEqual(AMQChannelLimit + expectedUniqueUnallocatedCount, total)
+    }
+
+    func sumUnallocated(accumulator: Int, current: Set<NSNumber>) -> Int {
+        return accumulator + (current.count == self.allocationsPerQueue ? 0 : 1)
     }
 
 }
