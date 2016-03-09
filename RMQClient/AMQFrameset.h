977bb174233fc6a53b29abab90c459347612bf13
Andrew Bruce <me@andrewbruce.net>
2016-03-09 10:18:56 +0000

    Remove unused Frameset property
    [#38]

--- RMQClient/AMQFrameset.h
+++ RMQClient/AMQFrameset.h
@@ -1,11 +1,10 @@
 #import "AMQValues.h"
 
 @interface AMQFrameset : MTLModel<AMQEncoding>
+@property (nonnull, nonatomic, copy, readonly) NSNumber *channelNumber;
 @property (nonnull, nonatomic, copy, readonly) id<AMQMethod>method;
 @property (nonnull, nonatomic, readonly) AMQContentHeader *contentHeader;
 @property (nonnull, nonatomic, readonly) NSArray<AMQContentBody *> *contentBodies;
-@property (nonnull, nonatomic, copy, readonly) NSNumber *channelNumber;
-@property (nonnull, nonatomic, readonly) NSArray *frames;
 - (nonnull instancetype)initWithChannelNumber:(nonnull NSNumber *)channelNumber
                                        method:(nonnull id<AMQMethod>)method
                                 contentHeader:(nonnull AMQContentHeader *)contentHeader
