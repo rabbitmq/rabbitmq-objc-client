cedd4d1013b2647f0dc7de9d8385f67aa9471fac
Andrew Bruce <me@andrewbruce.net>
2016-04-26 09:17:16 +0100

    Add default labels to Consumer params
    [#118335067]

--- RMQClient/RMQConsumer.h
+++ RMQClient/RMQConsumer.h
@@ -2,4 +2,4 @@
 #import "RMQDeliveryInfo.h"
 #import "RMQMessage.h"
 
-typedef void (^RMQConsumer)(RMQDeliveryInfo * _Nonnull, RMQMessage * _Nonnull);
+typedef void (^RMQConsumer)(RMQDeliveryInfo * _Nonnull deliveryInfo, RMQMessage * _Nonnull message);
